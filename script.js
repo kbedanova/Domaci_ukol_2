f64d4172bf94d9aa2c3a8eb3ade19d24d36179f6
Kateřina Bedáňová <nesporovakaterina@gmail.com>
2024-05-04 21:54:02 +0200

    Soubory k domácímu úkolu číslo 2.

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ const jmeno = prompt("Zadej své křestní jmeno bez diakritiky.")
 const prijmeni = prompt("Zadej své přijmení bez diakritiky")
 const jmenoMale = jmeno.toLowerCase()
 const prijmeniMale = prijmeni.toLowerCase()
-//console.log(jmenoMale)
 
 const jmenoMaleEmail = jmenoMale.trim()
 const prijmeniMaleEmail = prijmeniMale.trim()
-//console.log(jmenoEmail);
-//console.log(prijmeniEmail)
 
 const jmenoMaleEmailVyrez = jmenoMaleEmail.slice(0,3)
 const prijmeniMaleEmailVyrez = prijmeniMaleEmail.slice(0,5)
